f2206247cedc4244559adcda499159a1a8e50572
michelia <michelia@seal.io>
2023-09-25 16:16:14 +0800

    update variable

--- variables.tf
+++ variables.tf
@@ -2,5 +2,26 @@
 # @group "Basic"
 variable "name" {
   type        = string
-  description = "name for test"
+  description = "name for deployed cluster"
+}
+# @label "Public Endpoint"
+# @group "Basic"
+variable "public_endpoint" {
+  type        = bool
+  default     = true
+  description = "expose node port access for the LLM service UI"
+}
+# @label "Min Replica"
+# @group "Basic"
+variable "min_replica" {
+  type        = number
+  default     = 1
+  description = "min replica for the LLM service, automatically scale the number of replicas for your LLM service within min and max based on the load"
+}
+# @label "Max Replica"
+# @group "Basic"
+variable "max_replica" {
+  type        = number
+  default     = 1
+  description = "max replica for the LLM service, automatically scale the number of replicas for your LLM service within min and max based on the load"
 }
